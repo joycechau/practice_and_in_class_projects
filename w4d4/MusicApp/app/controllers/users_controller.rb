24160958056838e55b82287320675b2159caed67
Joyce Chau <jyc.chau@gmail.com>
2016-12-08 12:29:14 -0800

    Complete Phase 0

--- w4d4/MusicApp/app/controllers/users_controller.rb
+++ w4d4/MusicApp/app/controllers/users_controller.rb
@@ -7,18 +7,22 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      session[:session_token] = @user.session_token
-      redirect_to users_url
+      login(@user)
+      redirect_to user_url(@user)
     else
       flash.now[:errors] = @user.errors.full_messages
       render :new
     end
   end
 
+  def show
+    render :show
+  end
+
   private
 
   def user_params
-    parms.require(:user).permit(:email, :password)
+    params.require(:user).permit(:email, :password)
   end
 
 end
