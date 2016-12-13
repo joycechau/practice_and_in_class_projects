8aeb7ba06b102306499b2e5f83eaf4ccc3d66f2d
Joyce Chau <jyc.chau@gmail.com>
2016-12-13 15:15:16 -0800

    Complete RailsLite Phase IIIa

--- w5d2/RailsLite/lib/controller_base.rb
+++ w5d2/RailsLite/lib/controller_base.rb
@@ -1,5 +1,6 @@
 require 'active_support'
 require 'active_support/core_ext'
+require 'active_support/inflector'
 require 'erb'
 require_relative './session'
 
@@ -38,6 +39,7 @@ class ControllerBase
   # use ERB and binding to evaluate templates
   # pass the rendered html to render_content
   def render(template_name)
+    
   end
 
   # method exposing a `Session` object
